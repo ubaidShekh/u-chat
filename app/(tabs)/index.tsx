d7afda76e6e3b684f8d5a9b4484eca69caebf9c3
ubaid shekh <ubaidjmi2022@gmail.com>
2026-09-07 23:58:52 +0530

    home page post ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,10 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
 import Feather from '@expo/vector-icons/Feather';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Octicons from '@expo/vector-icons/Octicons';
 import { StatusBar } from 'expo-status-bar';
 import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+
 
 
 
@@ -60,6 +63,43 @@ const stories = [
     hasStory: true,
   },
 ];
+
+const posts = [
+  {
+    id: '1',
+    username: 'joshua_l',
+    profileImage: 'https://i.pravatar.cc/150?img=12',
+    location: 'Tokyo, Japan',
+    image: 'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf',
+    likes: 44686,
+    likedBy: 'craig_love',
+    caption: 'The game in Japan was amazing and I want to share some photos',
+  },
+
+  {
+    id: '2',
+    username: 'karenne',
+    profileImage: 'https://i.pravatar.cc/150?img=47',
+    location: 'New York, USA',
+    image: 'https://images.unsplash.com/photo-1493246507139-91e8fad9978e',
+    likes: 28341,
+    likedBy: 'zackjohn',
+    caption: 'Beautiful places and unforgettable moments.',
+  },
+
+  {
+    id: '3',
+    username: 'zackjohn',
+    profileImage: 'https://i.pravatar.cc/150?img=11',
+    location: 'California, USA',
+    image: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e',
+    likes: 15782,
+    likedBy: 'joshua_l',
+    caption: 'Nothing better than watching the sunset.',
+  },
+];
+
+
   return (
   <>
   <StatusBar style="auto" />
@@ -77,6 +117,15 @@ const stories = [
  </View>
   <View style={{height:0.5,backgroundColor:'#ccc',}}/>
 
+
+
+// posts
+<FlatList
+data={posts}
+keyExtractor={(item) => item.id}
+showsVerticalScrollIndicator={false}
+bounces={false}
+ListHeaderComponent={<>
   //stories
 <FlatList 
 data={stories}
@@ -84,11 +133,12 @@ keyExtractor={(item) => item.id}
 horizontal
 showsHorizontalScrollIndicator={false}
 bounces={false}
-style={{ height: 120 }}
+
+
 
 renderItem={({item}) => (
 <>
-<View style={{padding:10,position: 'relative'}}>
+<View style={{padding:10,position: 'relative',}}>
   <Image source={{ uri: item.image }} style={{ width: 70, height: 70, borderRadius: 35,  borderWidth: item.hasStory ? 2 : 0, borderColor: item.hasStory ? '#ff8501' : 'transparent',}} />
   {item.isLive && (
     <View style={{  backgroundColor: '#a83b3b', borderRadius: 4, paddingHorizontal: 5, paddingVertical: 2, marginTop:-10,position: 'absolute', right: 30, top: 75, }}>
@@ -96,15 +146,47 @@ renderItem={({item}) => (
     </View>
   )}
   <Text style={{ fontSize: 12, marginTop: 10 }}>{item.username}</Text>
- 
+  
+  
 </View>
- 
+
+ <View style={{height:1,backgroundColor:'#bbb',}}/>
 
 </>
 
 )}
 />
-<View style={{height: 0.5,backgroundColor:'#bbb',marginBottom:585}}/>
+
+</>}
+renderItem={({ item }) => (<>
+<View>
+<View style={{flexDirection:'row',justifyContent:'space-between',padding:10,alignItems:'center'}}>
+  <View style={{flexDirection:'row',}}>
+  <Image source={{uri:item.profileImage}} height={40} width={40} style={{borderRadius:20}}/>
+<View style={{marginLeft:6}}>
+  <Text style={{fontWeight:'600'}}>{item.username}</Text>
+  <Text style={{fontSize:11}}>{item.location}</Text>
+
+</View>
+
+</View>
+<AntDesign name="ellipsis" size={22} color="black" />
+
+</View>
+
+<View>
+<Octicons name="heart" size={24} color="black" />
+<AntDesign name="message" size={24} color="black" />
+ <MaterialIcons name="send" size={24} color={iconColor} />
+</View>
+
+
+<Image source={{uri:item.image}} style={{height:400,width:'100%'}}/>
+<Text>{item.caption}</Text>
+<Text>{item.likes} likes</Text>
+<Text>Liked by {item.likedBy}</Text></View>
+</>)}
+/>
 
 
  </>
