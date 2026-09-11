e59050252f0ade053038c17c1c0b5f3229139f9a
ubaid shekh <ubaidjmi2022@gmail.com>
2026-09-11 22:45:42 +0530

    home page post ui

--- app/(tabs)/Liked.tsx
+++ app/(tabs)/Liked.tsx
@@ -1,3 +1,43 @@
+
+import { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 export default function Liked(){
+  const Tab = [
+    {
+      id:"Following",
+      Tab:"Following",
+    },
+    {
+      id:"You",
+      Tab:"You"
+    }
+    , 
+  ]
+
+  const [activeTab,setActiveTab] = useState<string>("You")
+  return(
+    <>
+    <View style={{ paddingTop:60,}}>
+     <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+       {Tab.map((item,intex)=>(
+        <>
+        <TouchableOpacity key = {item.id} style={{padding:16,paddingHorizontal:40}} activeOpacity={0.9} onPress={()=>{setActiveTab(item.Tab)}}>
+          <Text style={{fontWeight:'600',color:item.id ===activeTab?'#111':'#777',letterSpacing:0.3}} >{item.Tab}</Text>
+
+        </TouchableOpacity>
+      
+       
+        </>
+      ))}
+      
+     </View>
+        <View style={{height:1.5,backgroundColor:'#ddd'}}/>
+
+        <Text style={{padding:16,color:'#555', fontWeight:'600',letterSpacing:0.3}}>Follow Requests</Text>
+         <View style={{height:0.5,backgroundColor:'#ddd'}}/>
+    </View>
+
+    </>
+  );
 
 }
