27e713e0a41f354c78578d221a4d49ef7e0d88b9
ubaid shekh <ubaidjmi2022@gmail.com>
2026-09-18 20:16:07 +0530

    working on profile page

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,100 +1,84 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
 
 export default function TabTwoScreen() {
+
+ const profileData:profileDataTypes[] = [
+  {
+    id:1,
+    profileImage:'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee',
+    totalPost:54,
+    followers:834,
+    following:162,
+    name:'jacob West',
+    bio:'Digital goodies designer @pixsellz Everything is designed',
+    hightlight:[
+      'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee'
+      ,'https://images.unsplash.com/photo-1507525428034-b723cf961d3e',
+      'https://images.unsplash.com/photo-1493246507139-91e8fad9978e',
+      'https://images.unsplash.com/photo-1470770841072-f978cf4d019e',
+      'https://images.unsplash.com/photo-1441974231531-c6227db76b6e',
+      'https://images.unsplash.com/photo-1501785888041-af3ef285b470'
+
+
+    ],
+    posts:[
+      'https://images.unsplash.com/photo-1519681393784-d120267933ba',
+      'https://images.unsplash.com/photo-1497250681960-ef046c08a56e',
+      'https://images.unsplash.com/photo-1500534623283-312aade485b7',
+      'https://images.unsplash.com/photo-1518837695005-2083093ee35b',
+      'https://images.unsplash.com/photo-1469474968028-56623f02e42e',
+      'https://images.unsplash.com/photo-1501854140801-50d01698950b',
+
+    ]
+
+
+  }
+ ]
+ interface profileDataTypes{
+  id:number,
+  profileImage:string,
+  totalPost:number,
+  followers:number,
+  following:number,
+  name:string,
+  bio:string,
+  hightlight:string[],
+  posts:string[]
+
+ }
+
+
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
-          }}>
-          Explore
-        </ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image
-          source={require('@/assets/images/react-logo.png')}
-          style={{ width: 100, height: 100, alignSelf: 'center' }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful{' '}
-          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
-            react-native-reanimated
-          </ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+   <>
+   <View style={{padding:16,paddingTop:60}}>
+  <View style={{flexDirection:'row',justifyContent:'space-between',}}>
+     <Text style={{fontWeight:'100',fontSize:1}}>blank</Text>
+   <View style={{flexDirection:'row',justifyContent:'center',alignContent:'center'}}>
+     <FontAwesome name="lock" size={16} color="black" style={{marginRight:5}} />
+     <Text style={{fontWeight:'600'}}>Jacob_w</Text>
+    <AntDesign name="down" size={16} color="black" />
+
+   </View>
+   <AntDesign name="menu" size={24} style={{fontWeight:'600'}} />
+  </View>
+
+  <FlatList
+  data={profileData}
+
+ renderItem={({item})=>(
+  <>
+  <View>
+    <Text>hdello from ubaid</Text>
+  </View>
+  </>
+ )}
+  />
+   </View>
+   </>
   );
 }
 
