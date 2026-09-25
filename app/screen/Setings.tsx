63cfefe7e5c76189966f6f1fd654663bcd0d7707
ubaid shekh <ubaidjmi2022@gmail.com>
2026-09-25 16:36:58 +0530

    working on seting page

--- app/screen/Setings.tsx
+++ app/screen/Setings.tsx
@@ -1,4 +1,7 @@
 import AntDesign from '@expo/vector-icons/AntDesign';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { Text, TextInput, View } from 'react-native';
 
@@ -13,17 +16,87 @@ export default function Setings(){
       <Text style={{fontWeight:600,color:'#666',fontSize:12,letterSpacing:0.2}}>Your account</Text>
     </View>
     // your account
-    <View style={{padding:16,flexDirection:'row',justifyContent:'space-between',alignContent:'center'}}>
-     <View style={{flexDirection:'row',justifyContent:'space-between'}}>
-       <MaterialIcons name="account-circle" size={24} color="black" />
-      <View style={{paddingHorizontal:16,paddingRight:40}}>
+    <View style={{padding:16,flexDirection:'row',alignItems:'center'}}>
+      <MaterialIcons name="account-circle" size={24} color="black" />
+     <View style={{flexDirection:'row'}}>
+       
+      <View style={{paddingHorizontal:16,width:320}}>
         <Text style={{fontWeight:'600',letterSpacing:0.2,color:'#111'}}>Accounts Center</Text>
-        <Text style={{fontSize:12,fontWeight:'500',color:'#666'}}>Password,security, personal details, connected experience, ad preferences</Text>
+        <Text style={{fontSize:12,fontWeight:'500',color:'#666'}}>Password,security, personal details, connected experience,</Text>
       </View>
      </View>
       <AntDesign name="right" size={20} color="#666" />
     </View>
+{/*// devider */}
+    <View style={{height:4,backgroundColor:'#e9e9e9'}}/> 
+
+    {/*how you use u-chat */}
+    <View style={{padding:16}}>
+      <Text  style={{fontWeight:600,color:'#666',fontSize:12,letterSpacing:0.2,}}>How you use u-chat</Text>
+      <View style={{paddingTop:16,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<View style={{flexDirection:'row',alignItems:'center'}}>
+<Feather name="save" size={24} color="black" />
+<Text  style={{fontWeight:'600',letterSpacing:0.2,color:'#111',marginLeft:16}}>Saved</Text>
+</View>
+
+      <AntDesign name="right" size={20} color="#666" />
+
+      </View>
+
+      <View style={{paddingTop:16,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<View style={{flexDirection:'row',alignItems:'center'}}>
+<EvilIcons name="archive" size={24} color="black" />
+<Text  style={{fontWeight:'600',letterSpacing:0.2,color:'#111',marginLeft:16}}>Archive</Text>
+</View>
+
+      <AntDesign name="right" size={20} color="#666" />
+
+      </View>
+
+      <View style={{paddingTop:16,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<View style={{flexDirection:'row'}}>
+<Feather name="activity" size={24} color="black" />
+<Text  style={{fontWeight:'600',letterSpacing:0.2,color:'#111',marginLeft:16}}>Your Activity</Text>
+</View>
+
+      <AntDesign name="right" size={20} color="#666" />
+
+      </View>
+      <View style={{paddingTop:16,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<View style={{flexDirection:'row',alignItems:'center'}}>
+<MaterialIcons name="notifications-none" size={24} color="black" />
+<Text  style={{fontWeight:'600',letterSpacing:0.2,color:'#111',marginLeft:16}}>Notifications</Text>
+</View>
+
+      <AntDesign name="right" size={20} color="#666" />
+
+      </View>
+      <View style={{paddingTop:16,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<View style={{flexDirection:'row',alignItems:'center'}}>
+<Ionicons name="timer-outline" size={24} color="black" />
+<Text  style={{fontWeight:'600',letterSpacing:0.2,color:'#111',marginLeft:16}}>Time management</Text>
+</View>
+
+      <AntDesign name="right" size={20} color="#666" />
+
+      </View>
+      <View style={{paddingTop:16,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+<View style={{flexDirection:'row',alignItems:'center'}}>
+<Feather name="tablet" size={24} color="black" />
+<Text  style={{fontWeight:'600',letterSpacing:0.2,color:'#111',marginLeft:16}}>U-chat for tablets</Text>
+</View>
+
+      <AntDesign name="right" size={20} color="#666" />
+
+      </View>
+    </View>
+
+    {/*// devider */}
+    <View style={{height:4,backgroundColor:'#e9e9e9'}}/> 
     
+    <View style={{padding:16}}>
+      <Text style={{fontWeight:'600',color:'#666',fontSize:12,letterSpacing:0.2}}>Subscriptions</Text>
+    </View>
     </>
   )
 }
